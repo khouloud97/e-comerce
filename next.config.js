c365048553943aadbc5ee5c7a46a51751e4a99da
khouloud97 <khouloudachour.97@gmail.com>
2021-06-28 11:07:50 +0100

    ok

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
    env: {
         "BASE_URL": "http://localhost:3000",
-        "MONGODB_URL": "YOUR_MONGODB_URL",
+        "MONGODB_URL": "mongodb+srv://khouloud:khouloud@cluster0.y8j09.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
         "ACCESS_TOKEN_SECRET": "YOUR_ACCESS_TOKEN_SECRET",
 }
