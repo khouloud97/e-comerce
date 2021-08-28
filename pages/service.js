53a8769152cee75515e77a33c8cb8d3968f8fb68
khouloud97 <45214298+khouloud97@users.noreply.github.com>
2021-08-28 11:24:25 +0100

    Update service.js

--- pages/service.js
+++ pages/service.js
@@ -92,6 +92,9 @@ export function Service(props) {
 								No SIRET : xxxx <br /> No TVA Intracommunautaire : xxxx
 								<br />
 								E-mail : c.renard@ontonomia.com
+								<br /> développé par
+									<br />
+								E-mail :khouloudachour.97@gmail.com
 								<br />
 								Tél :
 								<img
